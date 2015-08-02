6b87a9e651b511c0641fa3d383012a54554c810c
Filatov Evgen <evgen.filatov@gmail.com>
2015-08-02 15:14:07 +0300

    updated names

--- benchmark.js
+++ benchmark.js
@@ -1,22 +1,22 @@
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
-var nativeRust = require('./src/native-rust');
-var nativeCppFfi = require('./src/native-cpp-ffi');
+var nativeRustFFI = require('./src/native-rust-ffi');
+var nativeCppFFI = require('./src/native-cpp-ffi');
 var nativeCpp = require('./src/native-cpp');
 var vanilla = require('./src/vanilla');
 
 suite
-	.add('  nativeRust.fibonacci(10)', function () {
-		nativeRust.fibonacci(10);
-	})
-	.add('     vanilla.fibonacci(10)', function () {
+	.add('vanilla.fibonacci(10)      ', function () {
 		vanilla.fibonacci(10);
 	})
-	.add('   nativeCpp.fibonacci(10)', function () {
+	.add('nativeRustFFI.fibonacci(10)', function () {
+		nativeRustFFI.fibonacci(10);
+	})
+	.add('nativeCpp.fibonacci(10)    ', function () {
 		nativeCpp.fibonacci(10);
 	})
-	.add('nativeCppFfi.fibonacci(10)', function () {
-		nativeCppFfi.fibonacci(10);
+	.add('nativeCppFFI.fibonacci(10) ', function () {
+		nativeCppFFI.fibonacci(10);
 	})
 	.on('complete', function () {
 		console.log(this[0].toString());
